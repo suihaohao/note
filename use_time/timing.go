55afa1f5c33ba0f204898d6a95173566349fbcf7
suihaohao <1255151021@qq.com>
2020-04-17 11:44:03 +0800

    定时方式改为传参；

--- use_time/timing.go
+++ use_time/timing.go
@@ -5,56 +5,55 @@ import (
 	"time"
 )
 
-//实现定时的五分钟方式
+//实现定时的五种方式
 
-func UseTimeAfter() {
-	afterTime := time.After(time.Second * 2)
+func UseTimeAfter(d time.Duration) {
+	afterTime := time.After(d)
 	<-afterTime
 	go func() {
 		logrus.Println("UseTimeAfter======")
 	}()
 	logrus.Println("UseTimeAfter", time.Now().Format("2006-01-02 15:04:05"))
-	UseTimeAfter()
+	UseTimeAfter(d)
 }
 
-func UseTimeSleep() {
-	time.Sleep(time.Second * 2)
+func UseTimeSleep(d time.Duration) {
+	time.Sleep(d)
 	go func() {
 		logrus.Println("UseTimeSleep======")
 	}()
 	logrus.Println("UseTimeSleep", time.Now().Format("2006-01-02 15:04:05"))
-	UseTimeSleep()
+	UseTimeSleep(d)
 }
 
-func UseNewTimer() {
-	newTime := time.NewTimer(time.Second * 2)
+func UseNewTimer(d time.Duration) {
+	newTime := time.NewTimer(d)
 	<-newTime.C
 	go func() {
 		logrus.Println("UseNewTimer======")
 	}()
 	logrus.Println("UseNewTimer", time.Now().Format("2006-01-02 15:04:05"))
-	UseNewTimer()
+	UseNewTimer(d)
 }
 
-func UseNewTicker1() {
-	newTime := time.NewTicker(time.Second * 2)
+func UseNewTicker1(d time.Duration) {
+	newTime := time.NewTicker(d)
 	for range newTime.C {
 		go func() {
 			logrus.Println("UseNewTicker1======")
 		}()
 		logrus.Println("UseNewTicker1", time.Now().Format("2006-01-02 15:04:05"))
 	}
-	UseNewTicker1()
+	UseNewTicker1(d)
 }
 
-func UseNewTicker2() {
-	newTime := time.NewTicker(time.Second * 2)
+func UseNewTicker2(d time.Duration) {
+	newTime := time.NewTicker(d)
 	<-newTime.C
 	go func() {
 		logrus.Println("UseNewTicker2======")
 	}()
 	logrus.Println("UseNewTicker2", time.Now().Format("2006-01-02 15:04:05"))
 
-	UseNewTicker2()
+	UseNewTicker2(d)
 }
-
